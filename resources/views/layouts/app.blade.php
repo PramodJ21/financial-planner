d0b6faf15db97075723f38927aafb62664f5e9e8
Pramod <pramodjoshi090@gmail.com>
2025-11-11 02:25:24 +0530

    footer remove

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,9 +68,6 @@
     <main class="flex-grow-1">
         @yield('content')
     </main>
-
-    @include('partials.footer')
-
     {{-- Bootstrap JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
